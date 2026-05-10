d10a6967fbef4a279fc0c75aa2d2b843d934836b
Michael <michael21289@hotmail.com>
2026-05-09 21:22:01 -0600

    Finish v0.2

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,28 +1,51 @@
-import { useState } from "react"
+import { useState, useRef } from "react"
+import activities from "../activities"
+import { Routes, Route, Navigate } from "react-router"
 import Results from "./Results"
 
 export default function Game(props) {
+    const gameLength = 10
 
-    const initialRandomIndex = Math.floor(Math.random() * props.activities.length)
+    const maxCost = props.cost === "$$$" ?
+        ["free", "$", "$$", "$$$"] :
+        props.cost === "$$" ? ["free", "$", "$$"] :
+            props.cost === "$" ? ["free", "$"] :
+                ["free"]
 
-    const [usedIndices, setUsedIndices] = useState([initialRandomIndex])
+    const activityWithCostAndLocation = activities.filter(activity =>
+        maxCost.includes(activity.cost) && props.location.includes(activity.location)
+    )
+
+    const [initialIndex, firstActivity] = (() => {
+        if (activityWithCostAndLocation.length === 0) return [null, null]
+        const idx = Math.floor(Math.random() * activityWithCostAndLocation.length)
+        return [idx, activityWithCostAndLocation[idx]]
+    })()
     const [acceptedActivies, setAcceptedActivies] = useState([])
-    const [currentActivity, setCurrentActivity] = useState(props.activities[initialRandomIndex])
+    const [gameFinished, setGameFinished] = useState(false)
+    const [currentActivity, setCurrentActivity] = useState(firstActivity)
+
+    const usedIndices = useRef(initialIndex !== null ? [initialIndex] : [])
+
+    const generateActivity = () => {
+        let randomIndex
+
+        do {
+            randomIndex = Math.floor(Math.random() * activityWithCostAndLocation.length)
+        } while (usedIndices.current.includes(randomIndex))
+
+        usedIndices.current.push(randomIndex)
+
+        setCurrentActivity(activityWithCostAndLocation[randomIndex])
+        if (usedIndices.current.length === gameLength + 1) {
+            setGameFinished(true)
+            console.log("game finished")
+        }
+    }
 
     const activitiesList = acceptedActivies.map(activity => (
         <li key={activity.id}>{activity.title}</li>
     ))
-
-    function generateActivity() {
-        let randomIndex
-
-        do {
-            randomIndex = Math.floor(Math.random() * props.activities.length)
-        } while (usedIndices.includes(randomIndex))
-
-        setUsedIndices(prevUsed => [...prevUsed, randomIndex])
-        setCurrentActivity(props.activities[randomIndex])
-    }
 
     function addActivity() {
         setAcceptedActivies(prevAccepted => [...prevAccepted, currentActivity])
@@ -30,25 +53,36 @@ export default function Game(props) {
 
     return (
         <>
-            {usedIndices.length >= props.activities.length ? <Results username={props.username} activitiesList={activitiesList}/> :
-                <section id="slider">
-                    <div className="activity-card">
-                        <p className="card-count">{usedIndices.length}/{props.activities.length}</p>
-                        <h2 className="activity-title">{currentActivity.title}</h2>
-                        <p className="activity-category">{currentActivity.category}</p>
-                        <p className="activity-description">{currentActivity.description}</p>
-                    </div>
-                    <div className="decision-buttons">
-                        <button onClick={() => { generateActivity(); addActivity(); }} >YES!</button>
-                        <button onClick={generateActivity} >NO!</button>
-                    </div>
-                    <div className="accepted-list">
-                        <ul>
-                            {activitiesList}
-                        </ul>
-                    </div>
-                </section>
-            }
+            {gameFinished && <Navigate to="/game/results/" replace={true} />}
+            <Routes>
+                <Route index element={
+                    <section id="game">
+                        <div className="activity-card">
+                            <p className="card-count">{usedIndices.current.length}/{gameLength}</p>
+                            <h2 className="activity-title">{currentActivity.title}</h2>
+                            <p className="activity-category">{currentActivity.category}</p>
+                            <p className="activity-description">{currentActivity.description}</p>
+                            <div className="quick-facts">
+                                <p className="location">Location: {currentActivity.location}</p>
+                                <p className="cost">Cost: {currentActivity.cost}</p>
+                            </div>
+                        </div>
+                        <div className="decision-buttons">
+                            <button onClick={() => { generateActivity(); addActivity(); }} >YES!</button>
+                            <button onClick={generateActivity} >NO!</button>
+                        </div>
+                        <div className="accepted-list">
+                            <ul>
+                                {activitiesList}
+                            </ul>
+                        </div>
+                    </section>
+                } />
+                <Route path="results/" element={<Results
+                    activitiesList={activitiesList}
+                    groupName={props.groupName}
+                />} />
+            </Routes>
         </>
     )
 }
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,10 +1,26 @@
+import { useRef } from "react"
+
 export default function Results(props) {
+    const promptRef = useRef(null)
+    function handleCopy() {
+        const textToCopy = props.activitiesList
+            .map(activity => activity.props.children)
+            .join('\n')
+        navigator.clipboard.writeText(textToCopy)
+        
+        promptRef.current.style.display = "block"
+    }
+
     return (
         <section>
-            <h2>{props.username}'s Activity List:</h2>
+            <h2>{props.groupName}'s Activity List:</h2>
             <ul className="accepted-list">
                 {props.activitiesList}
             </ul>
+            <button onClick={handleCopy}>
+                Copy to Clipboard
+            </button>
+            <p ref={promptRef} style={{display: "none", marginTop: "1rem", fontSize: "0.7rem"}}>Text copied</p>
         </section>
     )
 }   
